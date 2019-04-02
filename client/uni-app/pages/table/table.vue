4e1b2f21f6445dc175ade713ec0bd677db8ec8fa
XiaoyiHuang <HXY.Marco@gmail.com>
2019-04-02 22:10:38 +0800

    Displaying hard-coded table info

--- client/uni-app/pages/table/table.vue
+++ client/uni-app/pages/table/table.vue
@@ -1,47 +1,64 @@
 <template>
-	<view>
-		<view class="uni-list">
-			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in table_data" :key="index">
-				<view class="uni-media-list">
-					<image class="uni-media-list-logo" :src="item.author_avatar"></image>
-					<view class="uni-media-list-body">
-						<view class="uni-media-list-text-top">{{item['sn']}}</view>
-						<view class="uni-media-list-text-bottom uni-ellipsis">{{item['questName']}}</view>
-					</view>
-				</view>
-			</view>
-		</view>
-	</view>
-</template>
+   <data-tables :data="table_data" :pagination-props="{ pageSizes: [5, 10, 15] }">
+     <el-table-column v-for="title in titles" :prop="title.prop" :label="title.label" :key="title.label">
+     </el-table-column>
+   </data-tables>
+ </template>
 
 <script>
 	import utils from '../../common/util.js'
 	import msg from '../../common/msg.js'
 	import config from '../../common/config.js'
+	
 	export default {
 		data() {
 			return {
 				table_name: '',
-				table_data: []
+				titles: [{
+					prop: "flow_no",
+					label: "NO."
+					}, {
+					prop: "content",
+					label: "Content"
+					}, {
+					prop: "flow_type",
+					label: "Type"
+				}],
+				table_data: [{
+					"content": "Water flood",
+					"flow_no": "FW201601010001",
+					"flow_type": "Repair",
+					"flow_type_code": "repair",
+					}, {
+					"content": "Lock broken",
+					"flow_no": "FW201601010002",
+					"flow_type": "Repair",
+					"flow_type_code": "repair",
+					}, {
+					"content": "Help to buy some drinks",
+					"flow_no": "FW201601010003",
+					"flow_type": "Help",
+					"flow_type_code": "help"
+				}]
 			};
 		},
 		onLoad(e) {
 			this.table_name = e['table_name']
-			uni.request({
-				url: msg.url(),
-				method: 'GET',
-				data: msg.get_table_data(this.$store.state.token, this.table_name),
-				success: res => {
-					for (var i = 0; i < res.data['data'].length; i++) {
-						this.table_data[i] = JSON.parse(res.data['data'][i])
-					}
-					console.log(this.table_data[0]['questName'])
-				},
-				fail: () => {
-					console.log("失败！token = " + this.$store.state.token)
-				},
-				complete: () => {}
-			});
+// 			uni.request({
+// 				url: msg.url(),
+// 				method: 'GET',
+// 				data: msg.get_table_data(this.$store.state.token, this.table_name),
+// 				success: res => {
+// 					for (var i = 0; i < res.data['data'].length; i++) {
+// 						this.table_data[i] = JSON.parse(res.data['data'][i])
+// 					}
+// 					console.log(this.table_data[0]['questName'])
+// 				},
+// 				fail: () => {
+// 					console.log("失败！token = " + this.$store.state.token)
+// 				},
+// 				complete: () => {}
+// 			});
 		}
 	}
 </script>
