39da78fa640810f628e5884ed15095c05f37a0c8
zhangzeqiang <zhangzeqiang@kuaishou.com>
2019-04-04 18:43:20 +0800

    commit

--- client/uni-app/common/config.js
+++ client/uni-app/common/config.js
@@ -3,43 +3,77 @@ var ExcelConfig_json =
 {
   "configs": [
     {
-			// excel表名
+      // excel表名
       "excel": "Quest.xlsx",
-			// sheet名
+      // sheet名
       "sheet": "任务|Quest",
-			// Redis hash的表名
+      // Redis hash的表名
       "redis_table": "QUEST",
-			// Redis hash的key值
+      // Redis hash的key值
       "redis_key": "sn",
-			// 点击跳转的导航页
-			"navigateTo": "../table/table",
-    },
-	  {
-			// excel表名
-			"excel": "Quest.xlsx",
-			// sheet名
-			"sheet": "任务目标|QuestGoal",
-			// Redis hash的表名
-			"redis_table": "QUESTGOAL",
-			// Redis hash的key值
-			"redis_key": "sn",
-			// 点击跳转的导航页
-			"navigateTo": "../table/table",
+      // 点击跳转的导航页
+      "navigateTo": "../quest/quest"
     },
     {
-			// excel表名
-			"excel": "Quest.xlsx",
-			// sheet名
-			"sheet": "对话表|Plot",
-			// Redis hash的表名
-			"redis_table": "PLOT",
-			// Redis hash的key值
-			"redis_key": "sn",
-			// 点击跳转的导航页
-			"navigateTo": "../table/table",
+      // excel表名
+      "excel": "Quest.xlsx",
+      // sheet名
+      "sheet": "任务目标|QuestGoal",
+      // Redis hash的表名
+      "redis_table": "QUESTGOAL",
+      // Redis hash的key值
+      "redis_key": "sn",
+      // 点击跳转的导航页
+      "navigateTo": "../table/table"
+    },
+    {
+      // excel表名
+      "excel": "Quest.xlsx",
+      // sheet名
+      "sheet": "对话表|Plot",
+      // Redis hash的表名
+      "redis_table": "PLOT",
+      // Redis hash的key值
+      "redis_key": "sn",
+      // 点击跳转的导航页
+      "navigateTo": "../table/table"
     }
   ],
-	// 基础配置
+  "triggers": [
+//    {
+//      // excel表名
+//      "excel": "Trigger.xlsx",
+//      // sheet名
+//      "sheet": "ERegionTriggerType"
+//    },
+//    {
+//      // excel表名
+//      "excel": "Trigger.xlsx",
+//      // sheet名
+//      "sheet": "EItemUseScript"
+//    },
+    {
+      // excel表名
+      "excel": "Trigger.xlsx",
+      // sheet名
+      "sheet": "EConditionType"
+    },
+//    {
+//      // excel表名
+//      "excel": "Trigger.xlsx",
+//      // sheet名
+//      "sheet": "EnumQuestGoalType"
+//    },
+//    {
+//      // excel表名
+//      "excel": "Trigger.xlsx",
+//      // sheet名
+//      "sheet": "EActionType"
+//    }
+  ],
+  // 任务类型
+  "EnumQuestType": ["1", "主线任务", "2", "结局任务", "3", "关系任务", "4", "奇遇任务"],
+  // 基础配置
   "defaultNames": [
     "cs",
     "type",
