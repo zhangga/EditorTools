34ca6ac5861a8d1dfa9fc4544481510466870210
liguo <liguo@kuaishou.com>
2019-04-08 21:08:53 +0800

    Update tab.vue

--- client/uni-app/pages/tab/tab.vue
+++ client/uni-app/pages/tab/tab.vue
@@ -1,96 +1,253 @@
 <template>
-	<view :class="{'my-tabs':true,'space-between':formatBe}">
-		<view  v-for="(item,index) in getModelData" :key="index" :class="{'tab-item':true,'active':activeIndex==index}" @tap="tap(index)">
-			{{item.label}}
+		<view>
+			<view style="flex:3;">
+				<el-tree :data="treeData":props="defaultProps" @node-click="handleNodeClick">
+				</el-tree>
+			</view>
+			<view style="flex:7;">
+				<view style="flex-direction: column;flex: 1;">
+					<el-tabs activeName="property">
+						<el-tab-pane label="任务属性" name="property">
+							<form @submit="propertySubmit">
+								<view>
+									<view style="flex: 1;">
+										<el-input name="taskID" placeholder="任务ID" v-model="taskID" />
+									</view>
+													
+									<view style="flex: 1;">
+										<el-input name="taskName" placeholder="任务名称" v-model="taskName" />
+									</view>
+								</view>
+								<view>
+									<view style="flex: 1;">
+										<el-select class="item-choose" v-model="value" size="medium">
+											<el-option v-for="item in options" :key="item" :value="item">
+											</el-option>
+										</el-select>
+									</view>			
+									<view style="flex: 1;">
+										<el-checkbox name="giveUp" v-model="checked">
+											可以手动放弃
+										</el-checkbox>
+									</view>
+								</view>
+					
+								<view>
+									<view style="flex: 1;">
+										<el-input name="seqID" placeholder="排序id" v-model="seqID" />
+									</view>
+													
+									<view style="flex: 1;">
+										<el-checkbox name="isHidden" v-model="isHidden">
+											隐藏任务
+										</el-checkbox>
+									</view>
+								</view>
+								<view>
+									<button formType="submit">保存</button>
+								</view>
+							</form>
+						</el-tab-pane>
+						<el-tab-pane label="任务接取" name="accept">
+							<form @submit="acceptSubmit">
+								<view>
+									<el-input name="levelMin" placeholder="等级下限" v-model="levelMin" />
+								</view>
+					
+								<view>
+									<el-input name="levelMax" placeholder="等级上限" v-model="levelMax" />
+								</view>
+					
+								<view>
+									<el-input name="preTaskID" placeholder="前置任务id" v-model="preTaskID" />
+								</view>
+					
+								<view>
+									<el-select v-model="career" size="medium">
+										<el-option v-for="item in careers" :key="item" :value="item">
+										</el-option>
+									</el-select>
+								</view>
+					
+								<view>
+									<el-select v-model="race" size="medium">
+										<el-option v-for="item in races" :key="item" :value="item">
+										</el-option>
+									</el-select>
+								</view>
+					
+								<view>
+									<el-input name="otherConditionID" placeholder="其他条件id" v-model="otherConditionID" />
+								</view>
+					
+								<view>
+									<el-input name="startNPC" placeholder="接受NPC" v-model="startNPC" />
+								</view>
+					
+								<view>
+									<el-input name="endNPC" placeholder="结束NPC" v-model="endNPC" />
+								</view>
+					
+								<view>
+									<el-input name="startPlotID" placeholder="接任务对话id" v-model="startPlotID" />
+								</view>
+					
+								<view>
+									<el-input name="endPlotID" placeholder="交任务对话id" v-model="endPlotID" />
+								</view>
+					
+								<view>
+									<el-input name="startNotEndPlotID" placeholder="已接未达目标对话id" v-model="startNotEndPlotID" />
+								</view>
+					
+								<view class="uni-btn-v">
+									<button formType="submit">保存</button>
+								</view>
+							</form>
+						</el-tab-pane>
+						<el-tab-pane label="任务目标" name="target">
+							111
+						</el-tab-pane>
+						<el-tab-pane label="任务完成" name="complete">
+							222
+						</el-tab-pane>
+					</el-tabs>
+				</view>
+			</view>
 		</view>
-		<view>{{index}}</view>
-		</div>
-	</view>
 </template>
 
 <script>
 	import store from "../../store/index.js"
 	export default {
-		props:['modelData','initIndex'],
 		data() {
 			return {
-				defaultInfo:{
-					data:[
-						{label:"任务属性",},
-						{label:"任务接取"},
-						{label:"任务目标"},
-						{label:"任务完成"}
-					],
+				treeData:[{
+					label:"主线任务",
+					children:[{
+						label:"item1.1",
+						},
+						{
+							label:"item1.2",
+						},
+						{
+							label:"item1.3",
+						}]
 				},
-				activeIndex:0,//默认索引
-			}
-		},
-		computed:{
-			getModelData(){
-				return this.modelData||this.defaultInfo.data
-			},
-			formatBe(){
-				return this.modelData
-						?this.modelData.length>4?true:false
-						:false
+				{
+					label:"结局任务",
+					children:[{
+						label:"item1.1",
+						},
+						{
+							label:"item1.2",
+						},
+						{
+							label:"item1.3",
+						}]	
+				},
+				{
+					label:"关系任务",
+					children:[{
+						label:"item1.1",
+						},
+						{
+							label:"item1.2",
+						},
+						{
+							label:"item1.3",
+						}]
+				},
+				{
+					label:"奇遇任务",
+					children:[{
+						label:"item1.1",
+						},
+						{
+							label:"item1.2",
+						},
+						{
+							label:"item1.3",
+						}]
+				}],
+				defaultProps: {
+					children: 'children',
+					label: 'label'
+				},
+				options: [
+					"主线任务",
+					"结局任务",
+					"关系任务",
+					"奇遇任务"
+				],
+				value: "主线任务",
+				taskID: "",
+				taskName: "",
+				checked: false,
+				seqID: "",
+				isHidden: false,
+
+				levelMin: "",
+				levelMax: "",
+				preTaskID: "",
+				careers: [
+					"职业1",
+					"职业2",
+					"职业3"
+				],
+				career: "职业1",
+				races: [
+					"种族1",
+					"种族2",
+					"种族3"
+				],
+				race: "种族1",
+				otherConditionID: "",
+				startNPC: "",
+				endNPC: "",
+				startPlotID: "",
+				endPlotID: "",
+				startNotEndPlotID: ""
 			}
 		},
 		methods: {
-			tap(index){
-				if(index!==this.activeIndex)this.$emit("change",index);
-				this.activeIndex=index;
+			propertySubmit(){
+				console.log(this.taskID)
+				console.log(this.taskName)
+				console.log(this.value)
+				console.log(this.checked)
+				console.log(this.seqID)
+				console.log(this.isHidden)
 			},
-			/**
-			 * @name 初始化activeIndex
-			 */
-			initActive(){
-				this.activeIndex=this.initIndex||this.activeIndex
+			acceptSubmit(){
+				console.log(this.levelMin)
+				console.log(this.levelMax)
+				console.log(this.preTaskID)
+				console.log(this.career)
+				console.log(this.race)
+				console.log(this.otherConditionID)
+				console.log(this.startNPC)
+				console.log(this.endNPC)
+				console.log(this.startPlotID)
+				console.log(this.endPlotID)
+				console.log(this.startNotEndPlotID)
+			},
+			handleNodeClick(data){
+				console.log(data.label);
 			}
-		},
-		created() {
-			this.initActive()
-		},
+		}
 	}
 </script>
-<style lang='scss'>
-	.my-tabs {
-		height: 30upx;
-		font-size: 20upx;
-		display: flex;
-		justify-content: space-around;
-		box-sizing: border-box;
-		border-top: 2upx solid #dddddd;
-		border-bottom: 2upx solid #dddddd;
-		min-width: 100%;
-		overflow-x: auto;
+<style>
+	view{
+		border: #000000 solid 1upx;
+		font-size: 15upx;
+		margin: 1upx;
 		
-		.tab-item{
-			line-height: 15upx;
-			padding: 2upx;
-			min-width: 40upx;
-			text-align: center;
-		}
-		.tab-item.active{
-			position: relative;
-			color: #3682FF;
-		}
-		.tab-item.active::after{
-			content: "";
-			position: absolute;
-			bottom: 0;
-			left:50%;
-			transform: translateX(-50%);
-			width: 100%;
-			border-bottom: 2upx solid #3682FF;
-			animation: test ease 1 1.5s;
-		}
+		display: flex;
+		flex-direction: row;
 	}
-	.my-tabs.space-between{
-		justify-content: space-between;
-	}
-	@keyframes test{
-		0%{width: 100%}
-		50%{width: 150%}
-		100%{width: 100%}
+	.el-input{
+		border: #111111;
 	}
 </style>
