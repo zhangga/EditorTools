c9f3b678f62db4e04ca930e02284bf8f1870c32b
XiaoyiHuang <HXY.Marco@gmail.com>
2019-04-09 10:42:23 +0800

    Change UI of quest-prop

--- client/uni-app/pages/quest/quest-prop.vue
+++ client/uni-app/pages/quest/quest-prop.vue
@@ -1,6 +1,18 @@
 <template>
 	<view>
-		<button type="default" class="login-page-btn" formType="reset">重置4</button>
+		<el-card class="box-card">
+			<view slot="header" class="clearfix">
+				<span class="header">任务属性</span>
+			</view>
+			<el-form ref="form" :model="form" label-width="100px">
+				<el-form-item label="任务名称">
+					<el-input placeholder="请输入String类型名称" class="text"></el-input>
+				</el-form-item>
+				<el-form-item label="任务描述">
+					<el-input placeholder="请输入String类型描述" type="textarea" :rows="2" class="text"></el-input>
+				</el-form-item>
+			</el-form>
+		</el-card>
 	</view>
 </template>
 
@@ -15,5 +27,28 @@
 </script>
 
 <style>
+  .text {
+      font-size: 14px;
+  }
+  
+  .header {
+      font-size: 50px;
+  }
 
+  .item {
+      margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+	  display: table;
+      content: "";
+  }
+  .clearfix:after {
+	  clear: both
+  }
+
+  .box-card {
+	  width: 480px;
+  }
 </style>
