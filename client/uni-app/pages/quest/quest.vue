3dd4e5357a883814810d2ab47337a37972cbd9e6
XiaoyiHuang <HXY.Marco@gmail.com>
2019-04-10 21:25:53 +0800

    Add search feature for navmenu

--- client/uni-app/pages/quest/quest.vue
+++ client/uni-app/pages/quest/quest.vue
@@ -8,7 +8,9 @@
 							<span>任务菜单</span>
 						</el-col>
 						<el-col :span="15">
-							<el-cascader placeholder="按照名称/ID搜索" filterable style="position: absolute; right: 10upx"></el-cascader>
+							<el-cascader :options="navmenuSearchOptions" placeholder="按照名称/ID搜索" 
+							filterable v-model="searchedMenuItem" style="position: absolute; right: 10upx"
+							@change="handleSearchChanges"></el-cascader>
 						</el-col>
 					</el-row>
 				</el-header>
@@ -38,9 +40,15 @@
 					<el-tab-pane :label='tabConfig[0]' :name='tabConfig[0]'>
 						<quest-prop v-bind:tableRowData="currSelectedQuestData" v-bind:questTypes="questTypes" v-if="hasSelectedRowData"/>
 					</el-tab-pane>
-					<el-tab-pane :label='tabConfig[1]' :name='tabConfig[1]'><quest-acpt v-bind:tableRowData="currSelectedQuestData"/></el-tab-pane>
-					<el-tab-pane :label='tabConfig[2]' :name='tabConfig[2]'><quest-goal v-bind:tableRowData="currSelectedQuestData"/></el-tab-pane>
-					<el-tab-pane :label='tabConfig[3]' :name='tabConfig[3]'><quest-comp v-bind:tableRowData="currSelectedQuestData"/></el-tab-pane>
+					<el-tab-pane :label='tabConfig[1]' :name='tabConfig[1]'>
+						<quest-acpt v-bind:tableRowData="currSelectedQuestData" v-if="hasSelectedRowData"/>
+					</el-tab-pane>
+					<el-tab-pane :label='tabConfig[2]' :name='tabConfig[2]'>
+						<quest-goal v-bind:tableRowData="currSelectedQuestData" v-if="hasSelectedRowData"/>
+						</el-tab-pane>
+					<el-tab-pane :label='tabConfig[3]' :name='tabConfig[3]'>
+						<quest-comp v-bind:tableRowData="currSelectedQuestData" v-if="hasSelectedRowData"/>
+						</el-tab-pane>
 				</el-tabs>
 				</el-main>
 			</el-container>
@@ -69,7 +77,9 @@
 				dataLoadComplete: false,
 				hasSelectedRowData: false,
 				questTypes: [],
-				currSelectedQuestData: null
+				currSelectedQuestData: null,
+				navmenuSearchOptions: [],
+				searchedMenuItem: null
 			};
 		},
 		computed: {
@@ -82,32 +92,35 @@
 		},
 		onLoad: function() {
 			// 任务页签的配置
-			this.tabConfig = config.ExcelConfig()['QuestTab'];
-			this.onLoadQuestBrief();
+			this.tabConfig = config.ExcelConfig()['QuestTab']
+			this.onLoadQuestBrief()
 		},
 		methods: {
 			handleOpen: function(key, keyPath) {
-				console.log(key);
+				console.log(key)
+			},
+			handleSearchChanges: function(value) {
+				if (value.length >= 1) {
+					this.openSelectedMenu(value[0])
+				}
+				
+				if (value.length >= 2) {
+					this.$refs.menu.activeIndex = value[1]
+					this.triggerItemClick(value[1])
+				}
 			},
 			onNavClick: function(index) {
 				this.mainActiveIndex = index;
 			},
 			onItemClick: function(e) {
+				this.triggerItemClick(e.index)
+			},
+			triggerItemClick: function(index) {
 				// 解析点击项ID
-				var splittedIndex = e.index.split('.')
+				var splittedIndex = index.split('.')
 				
-				console.log("当前顶级菜单" + splittedIndex[0])
-				
-				// 关闭之前展开的顶级菜单（保留当前激活项目对应的顶级菜单）
-				for (var i = 1; i <= this.items.length; i++) {
-					if (i != splittedIndex[0]) {
-						console.log("关闭顶级菜单" + i)
-						this.$refs.menu.close(String(i))
-					}
-					else {
-						this.$refs.menu.open(String(i))
-					}
-				}
+				// 打开对应顶级菜单
+				this.openSelectedMenu(splittedIndex[0])
 				
 				this.activeSn = splittedIndex[2]
 				uni.request({
@@ -133,21 +146,59 @@
 					method: 'GET',
 					data: msg.get_all_quest_brief(this.$store.state.token),
 					success: res => {
-						this.items = res.data['data'];
+						this.items = res.data['data']
 						// 排序
 						for (let i = 0; i < this.items.length; i++) {
 							this.questTypes.push(this.items[i]['text'])
 							let children = this.items[i]['children']
 							children.sort(function(a, b) {
-								return a['id'] - b['id'];
+								return a['id'] - b['id']
 							});
 						}
 						
-						this.activeTab = this.tabConfig[0];
-						this.dataLoadComplete = true;
+						this.activeTab = this.tabConfig[0]
+						this.dataLoadComplete = true
+						
+						this.formatSearchOptions();
 					}
 				});
 			},
+			formatSearchOptions: function() {
+				if (this.items.length == 0) {
+					return
+				}
+				
+				for (var i = 0; i < this.items.length; i++) {
+					var topLevelOption = {"value": '', "label": '', "children": []}
+					topLevelOption['value'] = String(i + 1)
+					topLevelOption['label'] = this.items[i]['text']
+					
+					for (var j = 0; j < this.items[i].children.length; j++) {
+						var lowLevelOption = {"value": '', "label": ''}
+						lowLevelOption['value'] = String(i + 1) + '.' + String(j + 1) + '.' + this.items[i].children[j]['id']
+						lowLevelOption['label'] = this.items[i].children[j]['text']
+						
+						topLevelOption['children'].push(lowLevelOption)
+					}
+					
+					if (topLevelOption['children'].length == 0) {
+						delete topLevelOption.children
+					}
+					
+					this.navmenuSearchOptions.push(topLevelOption)
+				}
+			},
+			openSelectedMenu: function(activatedIndex) {
+				// 关闭之前展开的顶级菜单，并保留当前激活项目对应的顶级菜单
+				for (var i = 1; i <= this.items.length; i++) {
+					if (i != activatedIndex) {
+						this.$refs.menu.close(String(i))
+					}
+					else {
+						this.$refs.menu.open(String(i))
+					}
+				}
+			}
 		}
 	}
 </script>
