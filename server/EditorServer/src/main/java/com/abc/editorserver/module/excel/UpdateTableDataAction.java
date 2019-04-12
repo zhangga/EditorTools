1f1c9b24aed93e138200df30805fc6f24d876f7f
XiaoyiHuang <HXY.Marco@gmail.com>
2019-04-12 17:49:33 +0800

    Add logging when receiving message from client side

--- server/EditorServer/src/main/java/com/abc/editorserver/module/excel/UpdateTableDataAction.java
+++ server/EditorServer/src/main/java/com/abc/editorserver/module/excel/UpdateTableDataAction.java
@@ -4,6 +4,7 @@ import com.abc.editorserver.manager.DataManager;
 import com.abc.editorserver.module.user.User;
 import com.abc.editorserver.msg.GameActionJson;
 import com.abc.editorserver.net.RequestData;
+import com.abc.editorserver.support.LogEditor;
 import com.alibaba.fastjson.JSONObject;
 
 /**
@@ -19,6 +20,7 @@ public class UpdateTableDataAction extends GameActionJson {
         String field = request.msg.getString("field");
         String value = request.msg.getString("value");
         int ret = DataManager.getInstance().updateTableData(table, sn, field, value);
+        LogEditor.serv.info("更新【" + table + "】表中SN为【" + sn + "】的记录的【" + field + "】列为：" + value);
         JSONObject msg = new JSONObject();
         msg.put("ret", ret);
         sendMsg(request.ctx, msg);
