098699b2b348881d0c1788a26814bb84bf252a65
zhangzeqiang <zhangzeqiang@kuaishou.com>
2019-04-18 11:22:34 +0800

    about

--- client/uni-app/pages/about/about.vue
+++ client/uni-app/pages/about/about.vue
@@ -1,8 +1,19 @@
 <template>
 	<view>
-		<el-carousel :interval="4000" type="card" height="250px">
-			<el-carousel-item v-for="item in 6" :key="item">
-				<h3>{{ item }}</h3>
+		<el-carousel :interval="3000" type="card" height="100upx">
+			<el-carousel-item v-for="item in 5" :key="item">
+				<view class="image-content">
+                    <image style="width: 200px; height: 200px; background-color: #eeeeee;" src="/static/logo.png"
+                        @error="imageError" v-if="item === 1"></image>
+					<image style="width: 200px; height: 200px; background-color: #eeeeee;" src="/static/au_huangxiaoyi.png"
+					    @error="imageError" v-if="item === 2"></image>
+					<image style="width: 200px; height: 200px; background-color: #eeeeee;" src="/static/au_liguo.png"
+					    @error="imageError" v-if="item === 3"></image>
+					<image style="width: 200px; height: 200px; background-color: #eeeeee;" src="/static/au_yulixiang.png"
+					    @error="imageError" v-if="item === 4"></image>
+					<image style="width: 200px; height: 200px; background-color: #eeeeee;" src="/static/au_zhangga.png"
+					    @error="imageError" v-if="item === 5"></image>
+                </view>
 			</el-carousel-item>
 		</el-carousel>
 		
